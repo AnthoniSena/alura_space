7ee34a10b03bf3ad1856a764090980ec056d7810
AnthoniSena <anthonisilvades@gmail.com>
2023-03-21 23:50:08 -0300

    Criando a logica de login e de cadastro de usuarios

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -24,4 +24,48 @@ class LoginForms(forms.Form):
         )
     )
 
-
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label = "Nome de Cadastro",
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+)
+    email = forms.EmailField(
+        label = "Email de Cadastro",
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: joaosilva@gmail.com"
+            }
+        )
+)
+    senha1 = forms.CharField(
+        label = "Senha",
+        required = True,
+        max_length = 40,
+        widget = forms.PasswordInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha2 = forms.CharField(
+        label = "Corfirme sua senha",
+        required = True,
+        max_length = 40,
+        widget = forms.PasswordInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
