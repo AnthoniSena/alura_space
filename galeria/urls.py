198af240d8126057ee85ca2c8092c044d365c3ba
ANTHONI SILVA DE SENA <anthoni.sena@volus.com>
2023-03-24 14:26:26 -0300

    Arrumando o base.html

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,8 +3,8 @@ from galeria.views import index, imagem, buscar, busca_tag
 
 urlpatterns = [
     path('', index, name='index'),
-    path('index', index, name='index'),
+    path('home', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name="buscar"),
-    path('tags/<str:tag>', busca_tag, name="tag")
+    path('tag/<str:tag>', busca_tag, name="tag")
 ]
