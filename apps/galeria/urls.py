d21bf71c90ec0afcf0b339120c21ba4c5a4ea1fe
AnthoniSena <anthonisilvades@gmail.com>
2023-03-26 02:33:07 -0300

    Fazendo o crud do projeto

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from apps.galeria.views import index, imagem, buscar, busca_tag
+from apps.galeria.views import \
+    index, imagem, buscar, busca_tag, nova_imagem, editar_imagem, deletar_imagem
 
 urlpatterns = [
     path('', index, name='index'),
     path('home', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name="buscar"),
-    path('tag/<str:tag>', busca_tag, name="tag")
+    path('tag/<str:tag>', busca_tag, name="tag"),
+    path('nova-imagem', nova_imagem, name="nova_imagem"),
+    path('editar-imagem/<int:foto_id>', editar_imagem, name="editar_imagem"),
+    path('deletar-imagem/<int:foto_id>', deletar_imagem, name="deletar_imagem")
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 
 from django.contrib import messages
 
@@ -13,7 +14,6 @@ def index(request):
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
-
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
@@ -32,7 +32,7 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, 'galeria/buscar.html', {"cards": fotografias})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def busca_tag(request, tag):
 
@@ -40,8 +40,44 @@ def busca_tag(request, tag):
         messages.error(request, "Usuário não logado")
         return redirect('login')
 
-    fotografias = Fotografia.objects.filter(publicada=True)
-
-    fotografias = fotografias.filter(categoria__exact=tag.upper())
+    fotografias = Fotografia.objects.filter(publicada=True, categoria = tag.upper())
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
+    
+    form = FotografiaForms
+    
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada')
+            return redirect('index')
+    
+    
+
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+
+    form = FotografiaForms(instance=fotografia)
+
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect('index')
+
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Deleção feita com sucesso')
+    return redirect('index')
